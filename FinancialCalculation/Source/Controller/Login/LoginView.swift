0035b7aa0b0efe4005c6a935d13b8aa8fa64e625
Renilson <renilson.moreiraf@gmail.com>
2021-06-11 17:10:09 -0300

    Adicionando botão de recuperação de senha

--- FinancialCalculation/Source/Controller/Login/LoginView.swift
+++ FinancialCalculation/Source/Controller/Login/LoginView.swift
@@ -16,11 +16,8 @@ class LoginView: UIView {
         textField.placeholder = "usuario"
         textField.keyboardType = UIKeyboardType.emailAddress
         textField.returnKeyType = UIReturnKeyType.default
-        textField.autocorrectionType = UITextAutocorrectionType.no
         textField.font = UIFont.systemFont(ofSize: 17)
         textField.borderStyle = UITextField.BorderStyle.roundedRect
-        textField.clearButtonMode = UITextField.ViewMode.whileEditing;
-        textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
         return textField
     }()
     lazy var textFieldSenha:UITextField = {
@@ -29,11 +26,9 @@ class LoginView: UIView {
         textField.placeholder = "senha"
         textField.keyboardType = UIKeyboardType.default
         textField.returnKeyType = UIReturnKeyType.done
-        textField.autocorrectionType = UITextAutocorrectionType.no
         textField.font = UIFont.systemFont(ofSize: 17)
+        textField.isSecureTextEntry = true
         textField.borderStyle = UITextField.BorderStyle.roundedRect
-        textField.clearButtonMode = UITextField.ViewMode.whileEditing;
-        textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
         return textField
     }()
     
@@ -65,13 +60,21 @@ class LoginView: UIView {
         button.layer.cornerRadius = 6
         button.backgroundColor = .mainBlack
         button.setTitle("LOGIN", for: .normal)
-        button.tintColor = .mainBackground
+        return button
+    }()
+    let buttonForgotPassword: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.frame = .zero
+        button.setTitleColor(.mainBlack, for: .normal)
+        button.contentHorizontalAlignment = .left
+        button.setTitle("esqueci minha senha", for: .normal)
         return button
     }()
     
     //MARK: Constants Buttons
     func setButtonLogin(){
-      // buttonLogin.addTarget(self, action: #selector(loginActionButton), for: .touchUpInside)
+       buttonLogin.addTarget(self, action: #selector(loginActionButton), for: .touchUpInside)
        
        addSubview(buttonLogin)
        NSLayoutConstraint.activate([
@@ -81,6 +84,31 @@ class LoginView: UIView {
         buttonLogin.heightAnchor.constraint(equalToConstant: 52)
        ])
    }
+    func setButtonForgotPassword(){
+        buttonForgotPassword.addTarget(self, action: #selector(forgotPasswordActionButton), for: .touchUpInside)
+       
+       addSubview(buttonForgotPassword)
+       NSLayoutConstraint.activate([
+        buttonForgotPassword.topAnchor.constraint(equalTo: self.buttonLogin.bottomAnchor, constant: 15),
+        buttonForgotPassword.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 12),
+        buttonForgotPassword.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -12),
+        buttonForgotPassword.heightAnchor.constraint(equalToConstant: 52)
+       ])
+   }
+    
+    //MARK: Actions Buttons
+    
+    @objc
+    func loginActionButton (sender: UIButton!) {
+        print("Tentando logar ")
+    }
+    
+    @objc
+    func forgotPasswordActionButton (sender: UIButton!) {
+        print("Usuario esqueceu a senha")
+        //print(textFieldUsuario.text!)
+    }
+    
     
     
     override init(frame: CGRect){
@@ -97,6 +125,7 @@ class LoginView: UIView {
         setTextFieldUsuario()
         setTextFieldSenha()
         setButtonLogin()
+        setButtonForgotPassword()
     }
     
 }
